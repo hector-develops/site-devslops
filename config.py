91c05d8f379c1b96e2e49411e61bdb16f221cb45
devslops <59939804+hector-develops@users.noreply.github.com>
2020-02-10 06:31:44 -0600

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class DevelopmentConfig(Config):
     MAIL_SERVER ='smtp.gmail.com'
     MAIL_PORT= 587
     MAIL_USERNAME='devslops.leon@gmail.com' 
-    MAIL_PASSWORD='HL20219503*L'
+    MAIL_PASSWORD='*******'
     MAIL_USE_TLS= True
     MAIL_USE_SSL= False 
 config = {
